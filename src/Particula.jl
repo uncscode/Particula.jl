1e49cf7d35ad60c3c52700e45261b80403afb61a
ngam <67342040+ngam@users.noreply.github.com>
2022-02-24 09:44:06 -0500

    fix greet

--- src/Particula.jl
+++ src/Particula.jl
@@ -2,15 +2,17 @@ module Particula
 
 using Unitful
 
-include("constants.jl")
 include("dviscosity.jl")
 include("inputting.jl")
 
 
 # Write your package code here.
 function greet()
-    print("Hello World! Oh wow, it is: ")
-    print(M_air, dviscosity(300*u"K"))
+
+    print("Hello World! \n")
+    print("Oh wow, molar mass of air is: ", M_air)
+    print("\t and viscosity is: ", µ(T = 300 * u"K"))
+
 end
 
 end # end the Particula module
