1364174f00713af28c838907c949636484f5534a
ngam <67342040+ngam@users.noreply.github.com>
2022-02-25 00:54:36 -0500

    formatting

--- test/test_meanfreepath.jl
+++ test/test_meanfreepath.jl
@@ -7,7 +7,7 @@ using Unitful, Test
 
 @testset "λg" begin
     @test P.λg() |> upreferred |> unit == u"m"
-    @test isapprox(P.λg() |> ustrip, 65e-9, atol=2e-9)
+    @test isapprox(P.λg() |> ustrip, 65e-9, atol = 2e-9)
     @test P.λg(T = 300) |> unit == u"m"
     @test size(P.λg(T = [300, 301, 302])) == (3,)
     @test_throws ArgumentError P.λg(T = 300 * u"C")
