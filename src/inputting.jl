a824a4a4f9c67f484ae58fbe2865f4455e54106f
ngam <67342040+ngam@users.noreply.github.com>
2022-02-24 20:14:13 -0500

    shorten doc

--- src/inputting.jl
+++ src/inputting.jl
@@ -1,15 +1,11 @@
-""" handling the inputs
 """
+    ℞X(X)
 
-using Unitful
+Ensure X in the base unit U (assuming unit(X)=U).
 
-"""
-    ℞T(T)
-
-Ensure T is in K.
-
-If T has units of K or equivalent, or if it has no units, it returns it in K.
-If T has any units other than K, it rases an ArgumentError. 
+If X has units of U or equivalent, or if it has no units,
+it returns it in U. If X has any units other than K, 
+it then rasies an ArgumentError. 
 
 # Notes
 It accepts vectors, but will throw an error if elements have different units.
@@ -18,16 +14,15 @@ It accepts vectors, but will throw an error if elements have different units.
 ```julia-repl
 julia> ℞T(300)
 300 K
-julia> ℞T(300*u"K")
-300 K
-julia> ℞T([290, 300, 310]*u"K")
-3-element Vector{Quantity{Int64, 𝚯, Unitful.FreeUnits{(K,), 𝚯, nothing}}}:
- 290 K
- 300 K
- 310 K
-julia> ℞T([290*u"K^-1", 300]*u"K")
-ERROR: ArgumentError: Input: in K (or equivalent) or unitless
-```
+````
+"""
+
+using Unitful
+
+"""
+    ℞T(T)
+
+Ensure T is in K.
 """
 function ℞T(T)
     if unit.(upreferred.(T)) == unit.(ustrip.(T) * u"K")
@@ -43,24 +38,6 @@ end
     ℞µ(µ)
 
 Ensure µ is in kg/m/s.
-
-If µ has units of kg/m/s or equivalent, or if it has no units,
-it returns it in kg/m/s. If µ has any units other than kg/m/s,
-it rases an ArgumentError. 
-
-# Notes
-It accepts vectors, but will throw an error if elements have different units.
-
-# Examples
-```julia-repl
-julia> ℞µ(1e-5)
-1.0e-5 kg m⁻¹ s⁻¹
-julia> ℞µ([1e-5,1.2e-5]*u"kg"/u"m"/u"s")
-2-element Vector{Quantity{Float64, 𝐌 𝐋⁻¹ 𝐓⁻¹, Unitful.FreeUnits{(kg, m⁻¹, s⁻¹), 𝐌 𝐋⁻¹ 𝐓⁻¹, nothing}}}:
- 1.0e-5 kg m⁻¹ s⁻¹
- 1.2e-5 kg m⁻¹ s⁻¹
-julia> ℞µ([1e-5*u"m",1.2e-5]*u"kg"/u"m"/u"s")
-ERROR: ArgumentError: Input: in kg/m/s (or equivalent) or unitless
 """
 function ℞µ(µ)
     if unit.(upreferred.(µ)) == unit.(ustrip.(µ) * u"kg" / u"m" / u"s")
